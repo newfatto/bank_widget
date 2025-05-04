ca4ace832e4a23e4a68ae549a0e46638b847e4bb
Ekaterina Chernyshova <newfatto@gmail.com>
2025-05-04 11:02:28 +0300

    Initial commit

---
+++ src/masks.py
@@ -0,0 +1,16 @@
+# Номер карты 7000792289606361
+# Номер счёта 73654108430135874305
+
+
+def get_mask_card_number(card_number: int) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+    return f"{str(card_number)[0:4]} {str(card_number)[4:6]}** **** {str(card_number)[12:]}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    return f"** {str(account_number)[-4:]}"
+
+
+print(get_mask_card_number(7000792289606361))
+print(get_mask_account(73654108430135874305))
