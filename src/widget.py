01346d7805bcdd2493d0fa26c31c86870c46b293
Ekaterina Chernyshova <newfatto@gmail.com>
2025-05-17 11:40:13 +0300

    Добавлены тесты к модулю Widget+get_date

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+import locale
+from datetime import datetime
+locale.setlocale(locale.LC_ALL, 'ru_RU')
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -31,13 +34,37 @@ def mask_account_card(info: str) -> str:
         raise TypeError ("Вы ввели некорректные данные")
 
 
-def get_date(date: str) -> str:
+def get_date(date_string: str) -> str:
     """Функция принимает на вход строку и возвращает дату в формате ДД.ММ.ГГГГ"""
-    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+    formats = [
+        "%Y-%m-%d",  # ISO 8601 формат (2024-03-11)
+        "%d/%m/%Y",  # Европейский формат (11/03/2024)
+        "%m/%d/%Y",  # Американский формат (03/11/2024)
+        "%Y/%m/%d",  # Японский формат (2024/03/11)
+        "%d %B %Y",  # С полным названием месяца (11 марта 2024)
+        "%d %b %Y",  # С сокращенным названием месяца (11 мар 2024)
+        "%Y-%m-%dT%H:%M:%S",  # ISO с временем (2024-03-11T02:26:18)
+        "%Y-%m-%d %H:%M:%S",  # ISO с временем (2024-03-11 02:26:18)
+        "%d.%m.%Y"  # Уже нужный формат (11.03.2024)
+    ]
 
+    for x in formats:
+        try:
+            date = datetime.strptime(date_string, x)
+            return date.strftime("%d.%m.%Y")
+        except ValueError:
+            continue
+
+    raise ValueError(f"Не удалось определить формат даты: {date_string}")
 
 if __name__ == "__main__":
-    print(mask_account_card("Счет 764"))
-    print(mask_account_card(input("Введите данные счёта или карты: ")))
-    print(get_date(input("Введите дату: ")))
-    print("Поздравляем, всё успешно!")
+
+    print(get_date("2024-03-11"))  # 11.03.2024
+    print(get_date("11/03/2024"))  # 11.03.2024
+    print(get_date("03/11/2024"))  # 03.11.2024
+    print(get_date("2024/03/11"))  # 11.03.2024
+    print(get_date("11 март 2024"))  # 11.03.2024
+    print(get_date("11 мар 2024"))  # 11.03.2024
+    print(get_date("2024-03-11T02:26:18"))  # 11.03.2024
+    print(get_date("11.03.2024"))  # 11.03.2024
+    print(get_date("Одиннадцатое марта две тысячи двадцать четвёртого года"))  # 11.03.2024
