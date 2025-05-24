dfea9ff34094d65872fd4dc56f1aeee3783d48de
Ekaterina Chernyshova <newfatto@gmail.com>
2025-05-24 15:30:12 +0300

    Добавлены все функции, тестирования, дополнен Readme

--- src/generators.py
+++ src/generators.py
@@ -1,115 +1,38 @@
-from typing import Optional, List, Iterable
-
-transactions = (
-    [
-            {
-                "id": 939719570,
-                "state": "EXECUTED",
-                "date": "2018-06-30T02:08:58.425572",
-                "operationAmount": {
-                    "amount": "9824.07",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод организации",
-                "from": "Счет 75106830613657916952",
-                "to": "Счет 11776614605963066702"
-            },
-            {
-                "id": 142264268,
-                "state": "EXECUTED",
-                "date": "2019-04-04T23:20:05.206878",
-                "operationAmount": {
-                    "amount": "79114.93",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод со счета на счет",
-                "from": "Счет 19708645243227258542",
-                "to": "Счет 75651667383060284188"
-            },
-            {
-                "id": 873106923,
-                "state": "EXECUTED",
-                "date": "2019-03-23T01:09:46.296404",
-                "operationAmount": {
-                    "amount": "43318.34",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод со счета на счет",
-                "from": "Счет 44812258784861134719",
-                "to": "Счет 74489636417521191160"
-            },
-            {
-                "id": 895315941,
-                "state": "EXECUTED",
-                "date": "2018-08-19T04:27:37.904916",
-                "operationAmount": {
-                    "amount": "56883.54",
-                    "currency": {
-                        "name": "USD",
-                        "code": "USD"
-                    }
-                },
-                "description": "Перевод с карты на карту",
-                "from": "Visa Classic 6831982476737658",
-                "to": "Visa Platinum 8990922113665229"
-            },
-            {
-                "id": 594226727,
-                "state": "CANCELED",
-                "date": "2018-09-12T21:27:25.241689",
-                "operationAmount": {
-                    "amount": "67314.70",
-                    "currency": {
-                        "name": "руб.",
-                        "code": "RUB"
-                    }
-                },
-                "description": "Перевод организации",
-                "from": "Visa Platinum 1246377376343588",
-                "to": "Счет 14211924144426031657"
-            }
-    ]
-)
+from typing import Any, Iterator, List
 
 
-# def filter_by_currency(transactions_list: List[dict], currency: str) -> Optional[List[dict]]:
-#     if transactions_list == []:
-#         raise ValueError("Список транзакций пуст")
-#     else:
-#         for transaction in transactions_list:
-#             if transaction.get('operationAmount', {}).get('currency', {}).get('name') != currency or transactions_list == []:
-#                 raise ValueError("Вы ввели некорректные данные")
-#             else:
-#                 return (transaction for transaction in transactions_list
-#                         if transaction.get('operationAmount', {}).get('currency', {}).get('name') == currency)
-
-def filter_by_currency(transactions_list: List[dict], currency: str) -> Iterable[:]:
+def filter_by_currency(transactions_list: List[dict], currency: str) -> Iterator[Any]:
+    """Функция принимает на вход список словарей, представляющих транзакции и возвращает итератор, который поочередно
+    выдает транзакции, где валюта операции соответствует заданной (например, USD)."""
     if not transactions_list:
         raise ValueError("Список транзакций пуст")
 
     try:
-        filtered_transactions = filter(lambda x: x['operationAmount']['currency']['name'] == currency, transactions_list)
+        filtered_transactions = filter(
+            lambda x: x["operationAmount"]["currency"]["name"] == currency, transactions_list
+        )
         for transaction in filtered_transactions:
             yield transaction
-
 
     except (KeyError, AttributeError):
         raise ValueError("Некорректный формат данных транзакций")
 
 
-if __name__ == "__main__":
+def transaction_descriptions(transactions_list: List[dict]) -> Iterator[Any]:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди"""
+    if not transactions_list:
+        raise ValueError("Список транзакций пуст")
 
-    usd_transactions = filter_by_currency(transactions, "USD")
-    for i in range(4):
-        print(next(usd_transactions))
+    for transaction in transactions_list:
+        yield transaction["description"]
 
 
+def card_number_generator(start: int, stop: int) -> Iterator[Any]:
+    """Функция-генератор принимает стартовое и конечное значение в виде цельных чисел, содержащих от 1 до 16 знаков,
+    и генерирует номера карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты."""
+    if start >= 1 and stop <= 9999999999999999:
+        for i in range(start, stop + 1):
+            num = f"{i:0{16}d}"
+            yield f"{num[:4]} {num[4:8]} {num[8:12]} {num[12:]}"
+    else:
+        raise ValueError("Задано некорректное значение")
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,26 +1,120 @@
 import pytest
-from src.generators import filter_by_currency
+
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 # Проверка работы функции filter_by_currency
+
 
 def test_filter_by_currency(transactions: list[dict]) -> None:
     """Проверка корректности работы функции при работе с заданной валютой"""
     gen = filter_by_currency(transactions, "USD")
-    assert next(gen) == {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'}
-    assert next(gen) == {'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}
-    assert next(gen) == {'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916', 'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658', 'to': 'Visa Platinum 8990922113665229'}
+    assert next(gen) == {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
+    assert next(gen) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    assert next(gen) == {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    }
 
 
-def test_filter_by_currency_incorrect(transactions) -> None:
+def test_filter_by_currency_incorrect(transactions: list[dict]) -> None:
     """Тестирование возникновения ошибки при передаче некорректной валюты"""
     with pytest.raises(StopIteration):
         iterator = filter_by_currency(transactions, "TNG")
         next(iterator)
 
 
-def test_filter_by_currency_empty_list() -> None:
+def test_filter_by_currency_empty_list(invalid_transactions: list[dict]) -> None:
+    """Тестирование возникновения ошибки при передаче пустого списка"""
+    with pytest.raises(ValueError):
+        iterator = filter_by_currency(invalid_transactions, "USD")
+        next(iterator)
+
+
+def test_filter_by_currency_invalid_list() -> None:
     """Тестирование возникновения ошибки при передаче пустого списка"""
     with pytest.raises(ValueError):
         iterator = filter_by_currency([], "USD")
         next(iterator)
 
+
+# Тестирование функции transaction_descriptions
+
+
+def test_transaction_descriptions(transactions: list[dict]) -> None:
+    """Проверка корректности работы функции"""
+    gen = transaction_descriptions(transactions)
+    assert next(gen) == "Перевод организации"
+    assert next(gen) == "Перевод со счета на счет"
+    assert next(gen) == "Перевод со счета на счет"
+    assert next(gen) == "Перевод с карты на карту"
+    assert next(gen) == "Перевод организации"
+
+
+def test_transaction_descriptions_incorrect(transactions: list[dict]) -> None:
+    """Тестирование возникновения ошибки при запросе транзакций, превышающем наличествующие в списке"""
+    with pytest.raises(StopIteration):
+        iterator = transaction_descriptions(transactions)
+        for i in range(10):
+            print(next(iterator))
+
+
+def test_transaction_descriptions_empty_list() -> None:
+    """Тестирование возникновения ошибки при передаче пустого списка"""
+    with pytest.raises(ValueError):
+        iterator = transaction_descriptions([])
+        next(iterator)
+
+
+# Тестирование функции-генератора card_number_generator
+
+
+def test_card_number_generator() -> None:
+    """Проверка корректности работы функции при работе с заданной валютой"""
+    gen = card_number_generator(67, 71)
+    assert next(gen) == "0000 0000 0000 0067"
+    assert next(gen) == "0000 0000 0000 0068"
+    assert next(gen) == "0000 0000 0000 0069"
+    assert next(gen) == "0000 0000 0000 0070"
+    assert next(gen) == "0000 0000 0000 0071"
+
+
+@pytest.mark.parametrize("start, stop, expected", [(1, 1, "0000 0000 0000 0001"),
+                                                   (9999999999999999, 9999999999999999, "9999 9999 9999 9999")])
+def test_card_number_generator_same(start, stop, expected) -> None:
+    """Тестирование возникновения ошибки при передаче одинакового значения stop и start (максимальные и минимальные)"""
+    assert next(card_number_generator(start, stop)) == expected
+
+
+def test_card_number_generator_incorrect_args() -> None:
+    """Тестирование возникновения ошибки при введении значения stop, превышающего значение start"""
+    with pytest.raises(StopIteration):
+        iterator = card_number_generator(100, 50)
+        print(next(iterator))
+
+
+def test_card_number_generator_incorrect() -> None:
+    """Тестирование возникновения ошибки при введении некорректного значения аргумента stop"""
+    with pytest.raises(ValueError):
+        iterator = card_number_generator(9999999999999999, 10999999999999999)
+        print(next(iterator))
