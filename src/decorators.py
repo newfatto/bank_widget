3cd0b6612ca5fc19a9019a494dc36e551cad6198
Ekaterina Chernyshova <newfatto@gmail.com>
2025-06-03 16:11:24 +0300

    Добавлены тесты для декоратора log и внесены изменения в Readme

--- src/decorators.py
+++ src/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 from typing import Any, Callable, Optional
 
 
-def log(filename: Optional[str] = None) -> Any | None:
+def log(filename: Optional[str] = None) -> Any:
     """Декоратор принимает в качестве аргумента имя файла, в который будут записываться логи
     и фиксирует в этом файле результат выполнения функции. В случае успешного выполнения записывает
     в логи имя функции и 'ок', в случае неуспешного - имя функции, тип ошибки и аргументы функции.
