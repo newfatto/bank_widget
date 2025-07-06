bcbd234f3e057acb03a345f731bacf843f7a56a3
Ekaterina Chernyshova <newfatto@gmail.com>
2025-07-06 14:48:07 +0300

    Добавлен модуль csv_excel_read и тесты к его функциямб обновлён файл README и проведена обработка линтерами

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
@@ -103,8 +105,7 @@ def test_card_number_generator() -> None:
     "start, stop, expected",
     [(1, 1, "0000 0000 0000 0001"), (9999999999999999, 9999999999999999, "9999 9999 9999 9999")],
 )
-
-def test_card_number_generator_same(start: int, stop: int, expected: Iterator[Any]) -> None:
+def test_card_number_generator_same(start: int, stop: int, expected: Any) -> None:
     """Тестирование возникновения ошибки при передаче одинакового значения stop и start (максимальные и минимальные)"""
     assert next(card_number_generator(start, stop)) == expected
 
